74f771d9a9a5cab1688b02cf2aeef296c25d5ba8
Lynn Zenn <1zenn0@proton.me>
2023-03-29 15:09:32 +0200

    feat: initial commit

--- source/background.js
+++ source/background.js
@@ -1,2 +1,24 @@
 // eslint-disable-next-line import/no-unassigned-import
-import './options-storage.js';
+import "./options-storage.js";
+
+console.log("chrome.browserAction");
+chrome.action.onClicked.addListener(async (tab) => {
+	const tabId = tab?.id;
+	if (!tabId) {
+		return;
+	}
+	await chrome.scripting.executeScript({
+		target: {
+			tabId,
+		},
+		func: () => {
+			document.dispatchEvent(
+				new CustomEvent("nostr_event", {
+					detail: {
+						data: "yes",
+					},
+				})
+			);
+		},
+	});
+});
